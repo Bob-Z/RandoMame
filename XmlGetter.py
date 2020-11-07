39e02267e4cf0e76f6eb6cbacd1271c29e137f7b
Bob-Z <carabobz@gmail.com>
2020-11-07 16:43:01 +0100

    Create XmlFilter

--- XmlGetter.py
+++ XmlGetter.py
@@ -1,5 +1,6 @@
 import subprocess
 import xml.etree.ElementTree as ElementTree
+from XmlFilter import XmlFilter
 
 
 def load_string():
@@ -19,52 +20,3 @@ def get():
     parser = ElementTree.XMLParser(target=target)
 
     return ElementTree.fromstring(stdout, parser=parser)
-
-
-class XmlFilter:
-    skip = False
-    defaultBuilder = ElementTree.TreeBuilder()
-
-    def start(self, tag, attrib):  # Called for each opening tag.
-        if tag == "rom":
-            self.skip = True
-            pass
-        elif tag == "device_ref":
-            self.skip = True
-            pass
-        elif tag == "chip":
-            self.skip = True
-            pass
-        elif tag == "display":
-            self.skip = True
-            pass
-        elif tag == "sound":
-            self.skip = True
-            pass
-        elif tag == "dipswitch":
-            self.skip = True
-            pass
-        elif tag == "port":
-            self.skip = True
-            pass
-        elif tag == "biosset":
-            self.skip = True
-            pass
-        else:
-            return self.defaultBuilder.start(tag, attrib)
-
-    def end(self, tag):  # Called for each closing tag.
-        if self.skip is False:
-            return self.defaultBuilder.end(tag)
-        else:
-            self.skip = False
-
-    def data(self, data):
-        if self.skip:
-            pass
-        else:
-            return self.defaultBuilder.data(data)
-
-    def close(self):  # Called when all data has been parsed.
-        print("close")
-        return self.defaultBuilder.close()
