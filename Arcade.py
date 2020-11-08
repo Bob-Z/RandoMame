e9222e13ef8c9f65bb56f93fc1f44f7614c2841c
Bob-Z <carabobz@gmail.com>
2020-11-09 00:04:42 +0100

    Skip BIOS

--- Arcade.py
+++ Arcade.py
@@ -6,15 +6,22 @@ def get(machine_count, root):
         rand = random.randrange(machine_count)
         machine = root[rand]
 
-        if machine.attrib["isdevice"] == "yes":
-            print("Skip device ", machine.attrib["name"])
-            continue
-        if machine.attrib["runnable"] == "no":
-            print("Skip non runnable ", machine.attrib["name"])
-            continue
-        if machine.attrib["ismechanical"] == "yes":
-            print("Skip mechanical ", machine.attrib["name"])
-            continue
+        if "isdevice" in machine.attrib:
+            if machine.attrib["isdevice"] == "yes":
+                print("Skip device ", machine.attrib["name"])
+                continue
+        if "isbios" in machine.attrib:
+            if machine.attrib["isbios"] == "yes":
+                print("Skip bios ", machine.attrib["name"])
+                continue
+        if "runnable" in machine.attrib:
+            if machine.attrib["runnable"] == "no":
+                print("Skip non runnable ", machine.attrib["name"])
+                continue
+        if "ismechanical" in machine.attrib:
+            if machine.attrib["ismechanical"] == "yes":
+                print("Skip mechanical ", machine.attrib["name"])
+                continue
 
         description = machine.find("description")
         year = machine.find("year")
