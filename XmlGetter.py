f13638803a6dc594b3e1938ddccdac44e4fa3bdf
Bob-Z <carabobz@gmail.com>
2020-11-14 21:09:37 +0100

    Give up on multiprocessing XML parsing since it ends up being slower

--- XmlGetter.py
+++ XmlGetter.py
@@ -1,6 +1,6 @@
 import subprocess
 import xml.etree.ElementTree as ElementTree
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 
 import Config
 from XmlMachineFilter import XmlMachineFilter
@@ -24,35 +24,24 @@ def load_soft_list():
     return out.communicate()
 
 
-class XmlGetter:
-    my_machine_list = None
-    my_soft_list = None
+def parse_machine_list(stdout):
+    print("Parsing MAME's machines list")
+    target = XmlMachineFilter()
+    parser = ElementTree.XMLParser(target=target)
+    return ElementTree.fromstring(stdout, parser=parser)
 
-    def parse_machine_list(self, stdout):
-        print("Parsing MAME's machines list start")
-        target = XmlMachineFilter()
-        parser = ElementTree.XMLParser(target=target)
-        self.my_machine_list = ElementTree.fromstring(stdout, parser=parser)
-        print("Parsing MAME's machines list finished")
 
-    def parse_soft_list(self, stdout):
-        print("Parsing MAME's softwares lists start")
-        self.my_soft_list = ElementTree.fromstring(stdout)
-        print("Parsing MAME's softwares lists finished")
+def parse_soft_list(stdout):
+    print("Parsing MAME's softwares lists")
+    return ElementTree.fromstring(stdout)
 
-    def get(self):
-        stdout, stderr = load_machine_list()
-        #parse_machine_process = Process(target=self.parse_machine_list, args=(stdout,))
-        #parse_machine_process.start()
-        self.parse_machine_list(stdout)
 
-        if Config.mode == 'softlist':
-            stdout, stderr = load_soft_list()
-            #parse_soft_process = Process(target=self.parse_soft_list, args=(stdout,))
-            #parse_soft_process.start()
-            self.parse_soft_list(stdout)
+def get():
+    stdout, stderr = load_machine_list()
+    machine_list = parse_machine_list(stdout)
 
-        #parse_machine_process.join()
-        #parse_soft_process.join()
+    if Config.mode == 'softlist':
+        stdout, stderr = load_soft_list()
+        soft_list = parse_soft_list(stdout)
 
-        return self.my_machine_list, self.my_soft_list
+    return machine_list, soft_list
