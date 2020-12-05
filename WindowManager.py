bac239f674784e43326fc2262e34a442dd97d046
Bob-Z <carabobz@gmail.com>
2020-12-05 18:29:43 +0100

    Manage window in threads

--- WindowManager.py
+++ WindowManager.py
@@ -1,7 +1,10 @@
 import datetime
 import os
 import subprocess
+import threading
 import time
+
+import pygame
 
 import Config
 import Desktop
@@ -14,15 +17,18 @@ import Sound
 from Desktop import DesktopClass
 from WindowPosition import WindowPosition
 
+my_machine_list = []
+my_soft_list = []
+desktop = DesktopClass()
+
 
 def start(machine_list, soft_list, window_qty=1):
-    window_position = WindowPosition()
+    global my_machine_list
+    my_machine_list = machine_list
+    global my_soft_list
+    my_soft_list = soft_list
 
-    out = []
-    title = []
-    desktop = DesktopClass()
-    command = []
-    date = []
+    window_position = WindowPosition()
 
     if Config.desktop is not None:
         desktop_info = [Config.desktop[0], Config.desktop[1], Config.desktop[2], Config.desktop[3]]
@@ -31,82 +37,96 @@ def start(machine_list, soft_list, window_qty=1):
 
     position = window_position.get(window_qty, desktop_info[0], desktop_info[1], desktop_info[2], desktop_info[3])
 
-    count = 20
+    for index in range(window_qty):
+        threading.Thread(target=manage_window, args=(index, position[index],)).start()
 
     while True:
-        for index in range(window_qty):
-            if len(out) > index:
-                if Config.mode == 'music':
-                    sdet = Sound.is_silence_detected()
-                    if sdet is True:
-                        out[index].kill()
-                        Sound.reset()
-
-                if date[index] < datetime.datetime.now():
-                    out[index].kill()
-                    Sound.reset()
-
-                while out[index].poll() is not None:
-                    out[index] = run_mame(command[index])
-
-                    while desktop.set_title(out[index].pid, title[index]) is False:
-                        if out[index].poll() is not None:
-                            break
-
-                    while desktop.set_position(out[index].pid, position[index]['pos_x'],
-                                               position[index]['pos_y'],
-                                               position[index]['width'], position[index]['height']) is False:
-                        if out[index].poll() is not None:
-                            break
-
-                    command[index], title[index] = get_command(machine_list, soft_list)
-
-                    date[index] = datetime.datetime.now() + datetime.timedelta(seconds=Config.timeout)
-
-                if count == 0:
-                    desktop.send_keyboard(out[index].pid)
-                    count = 10
-                else:
-                    count = count - 1
-
-            else:
-                first_command, first_title = get_command(machine_list, soft_list)
-                out.append(run_mame(first_command))
-                while desktop.set_title(out[index].pid, first_title) is False:
-                    if out[index].poll() is not None:
-                        break
-
-                while desktop.set_position(out[index].pid, position[index]['pos_x'],
-                                           position[index]['pos_y'],
-                                           position[index]['width'], position[index]['height']) is False:
-                    if out[index].poll() is not None:
-                        break
-
-                next_command, next_title = get_command(machine_list, soft_list)
-                command.append(next_command)
-                title.append(next_title)
-                delay_start = Config.timeout / Config.windows_quantity
-                date.append(
-                    datetime.datetime.now() + datetime.timedelta(seconds=Config.timeout) + datetime.timedelta(
-                        seconds=(index * delay_start)))
-
-                if Config.mode == 'music':
-                    Sound.reset()
-
-                time.sleep(0.1)
+        event = pygame.event.wait()
+        if event.type == pygame.KEYDOWN:
+            exit(0)
 
 
-def get_command(machine_list, soft_list):
-    if Config.mode == "arcade":
-        return ModeArcade.get(machine_list)
-    elif Config.mode == "softlist":
-        return ModeSoftList.get(machine_list, soft_list)
-    elif Config.mode == "all":
-        return ModeAll.get(machine_list, soft_list)
-    elif Config.mode == "selected softlist":
-        return ModeSelectedSoftList.get(machine_list, soft_list, Config.selected_softlist)
-    elif Config.mode == "music":
-        return ModeMusic.get(soft_list)
+def manage_window(index, position):
+    global desktop
+
+    first_command, first_title = get_command()
+    out = run_mame(first_command)
+    while desktop.set_title(out.pid, first_title) is False:
+        if out.poll() is not None:
+            break
+
+    while desktop.set_position(out.pid, position['pos_x'],
+                               position['pos_y'],
+                               position['width'], position['height']) is False:
+        if out.poll() is not None:
+            break
+
+    command, title = get_command()
+    delay_start = Config.timeout / Config.windows_quantity
+    date = datetime.datetime.now() + datetime.timedelta(seconds=Config.timeout) + datetime.timedelta(
+        seconds=(index * delay_start))
+
+    if Config.mode == 'music':
+        Sound.reset()
+
+    count = 0
+    while True:
+        if Config.mode == 'music':
+            silence_detected = Sound.is_silence_detected()
+            if silence_detected is True:
+                out.kill()
+                Sound.reset()
+
+        if date < datetime.datetime.now():
+            out.kill()
+            Sound.reset()
+
+        while out.poll() is not None:
+            count = 0
+            out = run_mame(command)
+
+            while desktop.set_title(out.pid, title) is False:
+                if out.poll() is not None:
+                    break
+
+            while desktop.set_position(out.pid, position['pos_x'],
+                                       position['pos_y'],
+                                       position['width'], position['height']) is False:
+                if out.poll() is not None:
+                    break
+
+            command, title = get_command()
+            date = datetime.datetime.now() + datetime.timedelta(seconds=Config.timeout)
+
+            count = 0
+
+        if count == 0:
+            desktop.send_keyboard(out.pid)
+            count = 20
+        else:
+            count = count - 1
+
+        time.sleep(0.1)
+
+
+gen_lock = threading.Lock()
+
+
+def get_command():
+    global my_machine_list
+    global my_soft_list
+    global gen_lock
+    with gen_lock:
+        if Config.mode == "arcade":
+            return ModeArcade.get(my_machine_list)
+        elif Config.mode == "softlist":
+            return ModeSoftList.get(my_machine_list, my_soft_list)
+        elif Config.mode == "all":
+            return ModeAll.get(my_machine_list, my_soft_list)
+        elif Config.mode == "selected softlist":
+            return ModeSelectedSoftList.get(my_machine_list, my_soft_list, Config.selected_softlist)
+        elif Config.mode == "music":
+            return ModeMusic.get(my_soft_list)
 
 
 def run_mame(command):
