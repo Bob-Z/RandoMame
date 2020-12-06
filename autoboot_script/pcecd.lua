19542cc9dbdc37201c4a91bb57881816a0fe2c48
Bob-Z <carabobz@gmail.com>
2020-12-06 22:24:38 +0100

    Release button in pcecd autoboot script

--- autoboot_script/pcecd.lua
+++ autoboot_script/pcecd.lua
@@ -3,5 +3,19 @@ local ports = manager:machine():ioport().ports[":JOY_P.0"]
 for field_name, field in pairs(ports.fields) do
     button[field_name] = field
 end
-button["P1 Run"]:set_value(1)
-button["P1 Run"]:set_value(0)
+
+local frame_num = 0
+local function process_frame()
+	frame_num = frame_num + 1
+
+	if frame_num < 25 then
+		button["P1 Run"]:set_value(1)
+	else
+		button["P1 Run"]:set_value(0)
+	end
+end
+
+emu.register_frame_done(process_frame)
+
+
+
