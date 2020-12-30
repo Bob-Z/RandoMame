dede3f8e898eb7dc6e342a912162f20ef1dc28d0
Bob-Z <carabobz@gmail.com>
2020-12-30 22:35:41 +0100

    Replace parec by PyAudio

--- SoundPulseAudio.py
+++ SoundPulseAudio.py
@@ -4,6 +4,7 @@ import struct
 import subprocess
 import threading
 import time
+import pyaudio
 
 silence_loop = 0
 sample_duration_sec = 0.1
@@ -18,22 +19,28 @@ monitor_silence_thread = None
 def monitor_silence():
     global silence_loop
     silence_loop = 0
+
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 44100
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
     while running is True:
-        try:
-            os.remove(tmp_file)
-        except FileNotFoundError:
-            pass
-
-        command = 'XDG_RUNTIME_DIR=/run/user/' + str(os.getuid()) + ' timeout ' + str(
-            sample_duration_sec) + 's parec > ' + tmp_file + ' 2>&1'
-        os.system(command)
-
-        with open(tmp_file, 'rb') as reader:
-            byte_array = reader.read(os.stat(tmp_file).st_size)
+        stream.start_stream()
+        byte_array = stream.read(CHUNK)
+        stream.stop_stream()
 
         count = int(len(byte_array) / 2)
 
-        if count > 85:
+        if count >= 1024:
             integers = struct.unpack('h' * count, byte_array)
 
             is_sound = False
@@ -58,6 +65,7 @@ def monitor_silence():
 
 
 def init():
+    print('Warning ! For smart sound to work,  make sure that your default (or fallback) "Input device" in PulseAudio\'s configuration is your output device\'s "monitor"')
     global monitor_silence_thread
     monitor_silence_thread = threading.Thread(target=monitor_silence)
     monitor_silence_thread.start()
