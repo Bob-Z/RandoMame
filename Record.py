98075e60f47ac98f38303dacec0fa484f4699607
Bob-Z <carabobz@gmail.com>
2021-10-02 22:26:15 +0200

    Fix record aspect file with vector display

--- Record.py
+++ Record.py
@@ -36,25 +36,29 @@ def create_aspect_ratio_file(item):
             ratio_string = ""
             additional_command = ["-snapsize", "1920x1440"]
         else:
-            width = float(display.attrib["width"])
-            height = float(display.attrib["height"])
-
-            if display.attrib["type"] == "raster":
-                # assume raster display is 4/3 ?
-
-                if width > height:  # 4/3
-                    ratio_string = "1920x1440"
-                else:  # 3/4
-                    ratio_string = "1080:1440"
+            if "width" not in display.attrib or "height" not in display.attrib:
+                ratio_string = ""
+                additional_command = ["-snapsize", "1920x1440"]
             else:
-                ratio1 = 2560.0 / width
-                ratio2 = 1440.0 / height
+                width = float(display.attrib["width"])
+                height = float(display.attrib["height"])
 
-                ratio = min(ratio1, ratio2)
-                output_width = width * ratio
-                output_height = height * ratio
+                if display.attrib["type"] == "raster":
+                    # assume raster display is 4/3 ?
 
-                ratio_string = str(int(output_width)) + ":" + str(int(output_height))
+                    if width > height:  # 4/3
+                        ratio_string = "1920x1440"
+                    else:  # 3/4
+                        ratio_string = "1080:1440"
+                else:
+                    ratio1 = 2560.0 / width
+                    ratio2 = 1440.0 / height
+
+                    ratio = min(ratio1, ratio2)
+                    output_width = width * ratio
+                    output_height = height * ratio
+
+                    ratio_string = str(int(output_width)) + ":" + str(int(output_height))
 
     with open(Config.record + "/" + '{:03d}'.format(index) + ".aspect", "w") as f:
         f.write(ratio_string)
