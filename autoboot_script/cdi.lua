c102ef33b2751d09764d5a718594f64caa25018e
Bob-Z <carabobz@gmail.com>
2023-01-14 17:35:34 +0100

    Fix cdi.lua autoboot script

--- autoboot_script/cdi.lua
+++ autoboot_script/cdi.lua
@@ -29,11 +29,11 @@ local function process_frame()
  		    manager.machine.video.throttled = previous_throttled
     		    manager.machine.video.frameskip = previous_frameskip
 
-                    button[":slave_hle:MOUSEBTN,1,64"]:set_value(1)
+                    button[":MOUSEBTN,1,64"]:set_value(1)
 	end
 
         if frame_num == boot_end_frame + 20 then
-	            button[":slave_hle:MOUSEBTN,1,64"]:set_value(0)
+	            button["MOUSEBTN,1,64"]:set_value(0)
         end
 end
 
