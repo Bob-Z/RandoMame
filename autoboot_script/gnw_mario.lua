0a0b44a154b3b49964beae27ec635c67c3c36423
Bob-Z <carabobz@gmail.com>
2023-07-21 11:51:37 +0200

    Clean-up gnw_mario.lua

--- autoboot_script/gnw_mario.lua
+++ autoboot_script/gnw_mario.lua
@@ -4,6 +4,8 @@ button = {}
 -- P1 Right Stick/Up	: 	:IN.0,2,55
 -- P1 Left Stick/Up	: 	:IN.0,4,59
 -- P1 Left Stick/Down	: 	:IN.0,8,60
+-- Game B	: 	:IN.1,2,8
+-- Game A	: 	:IN.1,4,7
 
 for i, j in pairs(manager.machine.ioport.ports) do
  for field_name, field in pairs(j.fields) do
@@ -14,143 +16,81 @@ for i, j in pairs(manager.machine.ioport.ports) do
 end
 
 frame_num = 0
-previous_left = {0,0,0}
-previous_right = {0,0,0}
--- name are up to down so index 1 is up and index 3 is down
-name_left = {"0.4.2","0.4.1","1.6.0"}
-name_right = {"1.9.3","1.7.1","1.11.1"}
-button_name_left = {":IN.0,4,59",":IN.0,8,60"}
-button_name_right = {":IN.0,2,55",":IN.0,1,56"}
-available_left = {}
-available_right = {}
-available_qty_left = 0
-available_qty_right = 0
-current_left = 0
-current_right = 0
-mario_svg_left = {"1.3.2","0.3.0","0.3.3"}
-mario_svg_right = {"1.10.3","0.10.2","0.11.2"}
-mario_left=0
-mario_right=0
-push_button_left = false
-push_button_time_left = 2
-push_button_right = false
-push_button_time_right = 2
+previous_crate_value = {{0,0,0},{0,0,0}}
+-- SVG names are up to down so index 1 is up and index 3 is down
+crate_svg_name = {{"0.4.2","0.4.1","1.6.0"},{"1.9.3","1.7.1","1.11.1"}}
+button_name = {{":IN.0,4,59",":IN.0,8,60"},{":IN.0,2,55",":IN.0,1,56"}}
+ready_crate_index_list = {{},{}}
+target_crate_index = {0,0}
+mario_svg = {{"1.3.2","0.3.0","0.3.3"},{"1.10.3","0.10.2","0.11.2"}}
 
 function process_frame()
         frame_num = frame_num + 1
 
-	-- check for new crate
-	for i = 1, 3 do
-		if previous_left[i] == 0 and manager.machine.output:get_value(name_left[i]) == 1 then
-			--print("Left detected ",i)
-			table.insert(available_left,1,i)
-			available_qty_left = available_qty_left + 1
-		end
-		previous_left[i] = manager.machine.output:get_value(name_left[i])
-
-		if previous_right[i] == 0 and manager.machine.output:get_value(name_right[i]) == 1 then
-			--print("Right detected ",i)
-			table.insert(available_right,1,i)
-			available_qty_right = available_qty_right + 1
-		end
-		previous_left[i] = manager.machine.output:get_value(name_left[i])
-		previous_right[i] = manager.machine.output:get_value(name_right[i])
+	-- auto start Game B
+	if frame_num == 60 then
+		button[":IN.1,2,8"]:set_value(1)
+	end
+	if frame_num == 65 then
+		button[":IN.1,2,8"]:set_value(0)
 	end
 
-	-- check for current crate cleared
-	if current_left ~= 0 then
-		if manager.machine.output:get_value(name_left[current_left]) == 0 then
-			--print("Left no more crate")
-			current_left = 0
+	for screen = 1, 2 do
+		-- check for new crate
+		for i = 1, 3 do
+			if previous_crate_value[screen][i] == 0 and manager.machine.output:get_value(crate_svg_name[screen][i]) == 1 then
+				table.insert(ready_crate_index_list[screen],1,i)
+			end
+			previous_crate_value[screen][i] = manager.machine.output:get_value(crate_svg_name[screen][i])
 		end
-	end
-	if current_right ~= 0 then
-		if manager.machine.output:get_value(name_right[current_right]) == 0 then
-			--print("Right no more crate")
-			current_right = 0
-		end
-	end
 
-	-- Set new crate
-	if current_left == 0 then
-		if available_qty_left > 0 then
-			-- get latest index and remove it
-			current_left = available_left[available_qty_left]
-			table.remove(available_left,available_qty_left)
-			available_qty_left = available_qty_left - 1
-			-- check if crate is still ther
-			if manager.machine.output:get_value(name_left[current_left]) == 0 then
-				print("Current crate is not here anymore ",current_left)
-				current_left = 0
+		-- check for target crate cleared
+		if target_crate_index[screen] ~= 0 then
+			if manager.machine.output:get_value(crate_svg_name[screen][target_crate_index[screen]]) == 0 then
+				--print("no more crate")
+				target_crate_index[screen] = 0
 			end
 		end
-	end
 
-	if current_right == 0 then
-		if available_qty_right > 0 then
-			-- get latest index and remove it
-			current_right = available_right[available_qty_right]
-			table.remove(available_right,available_qty_right)
-			available_qty_right = available_qty_right - 1
-			-- check if crate is still ther
-			if manager.machine.output:get_value(name_right[current_right]) == 0 then
-				print("Current crate is not here anymore ",current_right)
-				current_right = 0
+		-- set target crate
+		if target_crate_index[screen] == 0 then
+			if #ready_crate_index_list[screen] > 0 then
+				-- get latest index and remove it
+				target_crate_index[screen] = ready_crate_index_list[screen][#ready_crate_index_list[screen]]
+				table.remove(ready_crate_index_list[screen],#ready_crate_index_list[screen])
+				-- check if crate is still there (may happen when truck leaves: crates about to fall are cleared)
+				if manager.machine.output:get_value(crate_svg_name[screen][target_crate_index[screen]]) == 0 then
+					--print("Current crate is not here anymore ",target_crate_index[screen])
+					target_crate_index[screen] = 0
+				end
 			end
 		end
-	end
 
-	-- Push buttons
-	-- where is Mario
-	for i = 1, 3 do
-		if manager.machine.output:get_value(mario_svg_left[i]) == 1 then
-			mario_left = i
-			break
+		-- Push buttons
+		-- where is Mario
+		for i = 1, 3 do
+			if manager.machine.output:get_value(mario_svg[screen][i]) == 1 then
+				mario_pos = i
+				break
+			end
 		end
-	end
-	mario_target_left = current_left
-	-- if no crate try to wait in the middle
-	if current_left == 0 then
-		mario_target_left = 2
-	end
-
-	if mario_target_left > mario_left then
-			button[button_name_left[2]]:set_value(1)
-	elseif mario_target_left < mario_left then
-			button[button_name_left[1]]:set_value(1)
-	else
-		button[button_name_left[1]]:set_value(0)
-		button[button_name_left[2]]:set_value(0)
-		push_button_left = false
-		push_button_time_left = 2
-	end
-
-	-- where is Mario
-	for i = 1, 3 do
-		if manager.machine.output:get_value(mario_svg_right[i]) == 1 then
-			mario_right = i
-			break
+		mario_target = target_crate_index[screen]
+		-- if no crate try to wait in the middle
+		if target_crate_index[screen] == 0 then
+			mario_target = 2
 		end
-	end
-	mario_target_right = current_right
-	-- if no crate try to wait in the middle
-	if mario_target_right == 0 then
-		mario_target_right = 2
-	end
-
-	if mario_target_right > mario_right then
-			button[button_name_right[2]]:set_value(1)
-	elseif mario_target_right < mario_right then
-			button[button_name_right[1]]:set_value(1)
-	else
-		button[button_name_right[1]]:set_value(0)
-		button[button_name_right[2]]:set_value(0)
-		push_button_right = false
-		push_button_time_right = 2
+	
+		if mario_target > mario_pos then
+				button[button_name[screen][2]]:set_value(1)
+		elseif mario_target < mario_pos then
+				button[button_name[screen][1]]:set_value(1)
+		else
+			button[button_name[screen][1]]:set_value(0)
+			button[button_name[screen][2]]:set_value(0)
+		end
 	end
 end
 
 subscription = emu.add_machine_frame_notifier(process_frame)
---emu.add_machine_reset_notifier(process_frame)
 
 
